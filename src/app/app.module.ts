907cf7db9f5c91227149dfd8ee1d1c504449b6ae
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-07 22:02:01 +0500

    Create navbar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './include/navbar/navbar.component';
+import { FooterComponent } from './include/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
