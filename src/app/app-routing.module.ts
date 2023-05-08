61ebfac4fcf9deedb43e38f98d35de0ece4c2db5
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-08 20:08:59 +0500

    Add routes for login, profile, dashboard and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 
 const routes: Routes = [
-  // {
-  //   path :'',
-  //   component:HomeComponent
-  // },
+  {
+    path :'login',
+    component: LoginComponent
+  },
+  {
+    path :'register',
+    component: RegisterComponent
+  },
+  {
+    path :'profile',
+    component: ProfileComponent
+  },
+  {
+    path :'**',
+    component: DashboardComponent
+  },
+  
 ];
 
 @NgModule({
