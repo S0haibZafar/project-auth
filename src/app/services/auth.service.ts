1bc3ec5338d1ee5715dfb3a1fa808a2cdaa4ddc4
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-19 07:04:07 +0500

    Call register api

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(data:any):Observable<any>{
+    return this.http.post('http://localhost:8080/auth/register',data);
+  }
+
 }
