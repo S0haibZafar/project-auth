c95eb5f4609e2ca6a6d576824e5ba5ae8965bfa6
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-24 07:43:32 +0500

    Create login and getProfile api

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -11,6 +11,19 @@ export class AuthService {
 
   register(data:any):Observable<any>{
     return this.http.post('http://localhost:8080/auth/register',data);
+  }
+
+  
+  login(data:any):Observable<any>{
+    return this.http.post('http://localhost:8080/auth/login',data);
+  }
+
+  
+  getProfile(data:any):Observable<any>{
+    let headers = {
+      'Authorization':"Bearer" +  localStorage.getItem('token')
+    }
+    return this.http.get('http://localhost:8080/auth/profile',{headers:headers});
   }
 
 }
