7872fcad9b1bdb689e5a1f305676650c4da8c957
S0haibZafar <sohaibzafar71@gmail.com>
2023-05-26 08:08:07 +0500

    Call getProfile data api

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  data!:any;
+
+  constructor(private auth : AuthService) { }
 
   ngOnInit(): void {
+    this.getProfile();
+  }
+
+  getProfile(){
+    this.auth.getProfile().subscribe((res)=>{
+      if(res.success){
+        this.data = res.data;
+      }
+    });
   }
 
 }
